5056e7837cc6b992a97a75d34e970688bbb5e1a4
bahar abolhasani <baharabolhasany2004@gmail.com>
2025-04-07 22:13:39 +0330

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,18 @@
+<?php 
+session_start(); 
+?>
+
+
+<nav>
+    <?php if (isset($_SESSION['user'])): ?>
+        Welcome, <?= htmlspecialchars($_SESSION['user']['username']) ?> |
+        <a href="index.php">Home</a> |
+        <a href="dashboard.php">Dashboard</a> |
+        <a href="logout.php">Logout</a>
+    <?php else: ?>
+        <a href="login.php">Login</a> |
+        <a href="register.php">Register</a>
+    <?php endif; ?>
+</nav>
+<hr>
 
