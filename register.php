7d9a78e250a2eb96bd57b1258dbca30f3f965be1
bahar abolhasani <baharabolhasany2004@gmail.com>
2025-04-07 22:16:39 +0330

    Update register.php

--- register.php
+++ register.php
@@ -1,33 +1,57 @@
 <?php
 session_start();
-if(isset($_SESSION['user'])){
-    header("location:dashboard.php ");
+
+if (isset($_SESSION['user'])) {
+    header("Location: dashboard.php");
     exit;
 }
-
 $users = [];
-if(file_exists("data/users.json")){
-    $users = json_decode(file_get_contents("data/users.json") , true);
+if (file_exists("data/users.json")) {
+    $users = json_decode(file_get_contents("data/users.json"), true);
 }
-
-// $errors = [];
-// if($_SERVER['REQUEST_METHOD'] === 'POST'){
-//     $username = trim($_POST['username']);
-//     $password = trim($_POST['password']);
-
-//     if(&username === '' || $password === ''){
-//         $errors = "username & password is required ";
-//     }
-//     foreach($users as$user){
-//         if($user['username'] === $username){
-//             $errors []= "username already taken";
-//             break; 
-//         }
-//     }
-//     if (empty($errors)){
-//         $newuser = 
-//     }
-//     }
-// }
+$errors = [];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+    if ($username === '' || $password === '') {
+        $errors[] = "Username and password are required.";
+    }
+    foreach ($users as $user) {
+        if ($user['username'] === $username) {
+            $errors[] = "Username already taken.";
+            break;
+        }
+    }
+    if (empty($errors)) {
+        $newUser = [
+            "id" => count($users) + 1,
+            "username" => $username,
+            "password" => $password 
+        ];
+        $users[] = $newUser;
+        file_put_contents("data/users.json", json_encode($users, JSON_PRETTY_PRINT));
+        header("Location: login.php");
+        exit;
+    }
+}
 ?>
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container">
+<h2>Register</h2>
+<form method="POST">
+    <label>Username:</label><br>
+    <input type="text" name="username"><br><br>
+    <label>Password:</label><br>
+    <input type="password" name="password"><br><br>
+    <input type="submit" value="Register">
+</form>
+<p>Already have an account? <a href="login.php">Login here</a></p>
+</div>
+</body>
+</html>
