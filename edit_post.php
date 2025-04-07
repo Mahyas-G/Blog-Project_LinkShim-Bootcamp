865de75b9958b7e041aa25f461394e795aa5aa0b
Mahyas-G <MahyasGolparian@gmail.com>
2025-04-07 17:35:14 +0000

    EditPost_notCompleted

--- edit_post.php
+++ edit_post.php
@@ -1,1 +1,14 @@
+<?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+    exit;
+}
 
+if (!isset($_GET['id'])) {
+    header("Location: dashboard.php");
+    exit;
+}
+
+$postId = (int)$_GET['id'];
+$posts = file_exists("data/posts.json") ? json_decode(file_get_contents("data/posts.json"), true) : [];
