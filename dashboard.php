8676e773fd1cfe675723f294031903f0ebab4449
Reza-abolghasemi2004 <only.rezaabolghasemi1383@gmail.com>
2025-04-07 22:07:26 +0330

    add and fix the dashboard.

--- dashboard.php
+++ dashboard.php
@@ -1,2 +1,50 @@
+<?php
+if (isset($_SESSION['user'])) {
+    header("location:login.php");
+    exit();
+}
+$username = $_SESSION['user']['username'];
+$posts = [];
+if (file_exists("posts.txt")) {
+    $posts = json_decode(file_get_contents("posts.txt"), true);
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container">
 
-// hello
+    <?php include 'includes/header.php'; ?>
+
+    <h2>Your Posts</h2>
+    <a href="add_post.php" style="text-align: center;">+ Add New Post</a>
+    <br>
+    <br>
+
+    <?php
+    $userPosts = [];
+    foreach ($posts as $post) {
+        if ($post['author'] === $username) {
+            $userPosts[] = $post;
+        }
+    }
+
+    if (empty($userPosts)) {
+        echo "<p>You haven't written any posts yet.</p>";
+    } else {
+        foreach ($userPosts as $post) {
+            echo "<div class='post'>";
+            echo "<h3>" . htmlspecialchars($post['title']) . "</h3>";
+            echo "<p><small>Created at: " . $post['created_at'] . "</small></p>";
+            echo "<p><a href='edit_post.php?id={$post['id']}'>Edit</a> | <a href='delete_post.php?id={$post['id']}'>Delete</a></p>";
+            echo "</div><hr>";
+        }
+    }
+    ?>
+</div>
+</body>
+</html>
