7d967e07229446f9bd0b3ec04f0431bc170b5465
Mahyas-G <MahyasGolparian@gmail.com>
2025-04-08 15:34:54 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start(); 
 $posts = file_exists("data/posts.json") ? json_decode(file_get_contents("data/posts.json"), true) : [];
 ?>
 
@@ -11,24 +12,26 @@ $posts = file_exists("data/posts.json") ? json_decode(file_get_contents("data/po
 <body>
 <div class="container">
     <?php include 'includes/header.php'; ?>
-<h2>All Blog Posts</h2>
+    <h2>All Blog Posts</h2>
 
-<?php
-if (empty($posts)) {
-    echo "<p>No posts yet.</p>";
-} else {
-    foreach (array_reverse($posts) as $post) {
-        echo "<div class='post'>";
-        echo "<h3>" . htmlspecialchars($post['title']) . "</h3>";
-        echo "<p><small>By " . htmlspecialchars($post['author']) . " on " . $post['created_at'] . "</small></p>";
-        echo "<p><a href='view_post.php?id={$post['id']}'>Read More</a></p>";
-        if ($post['author'] == $_SESSION['user']['username']) {
-            echo "<p><a href='edit_post.php?id={$post['author']}'>Edit</a> </p>";
+    <?php
+    if (empty($posts)) {
+        echo "<p>No posts yet.</p>";
+    } else {
+        foreach (array_reverse($posts) as $post) {
+            echo "<div class='post'>";
+            echo "<h3>" . htmlspecialchars($post['title']) . "</h3>";
+            echo "<p><small>By " . htmlspecialchars($post['author']) . " on " . $post['created_at'] . "</small></p>";
+            echo "<p><a href='view_post.php?id={$post['id']}'>Read More</a></p>";
+
+            if (isset($_SESSION['user']) && $post['author'] === $_SESSION['user']['username']) {
+                echo "<p><a href='edit_post.php?id={$post['id']}'>Edit</a></p>";
+            }
+
+            echo "</div><hr>";
         }
-        echo "</div><hr>";
     }
-}
-?>
+    ?>
 </div>
 </body>
 </html>
